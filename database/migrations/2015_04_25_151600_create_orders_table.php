3b43ab870766dee0dd6b8c7bf22a39708317abb6
razat91 <azat-91knu@mail.ru>
2015-04-25 21:45:37 +0600

    Migrate table product and order

---
+++ database/migrations/2015_04_25_151600_create_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('orders', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('user_id');
+            $table->text('name');
+            $table->text('phone');
+            $table->integer('options');
+            $table->integer('count');
+            $table->date('delivery_date');
+            $table->dateTime('delivery_time');
+            $table->text('desc');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('orders');
+	}
+
+}
